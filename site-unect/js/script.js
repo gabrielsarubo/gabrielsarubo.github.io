0f7cf551007ad8e919ff5a47fe26f91fc04d3fda
Gabriel <gabrielsarubo@hotmail.com>
2021-03-10 21:38:55 -0300

    New version march 10 at 9pm

--- site-unect/js/script.js
+++ site-unect/js/script.js
@@ -21,36 +21,3 @@ window.onscroll = () => {
     header_mobile.classList.remove("header-fixo");
   }
 };
-
-// Slider
-const proximo = document.querySelector("#depois");
-const anterior = document.querySelector("#antes");
-const listaDepoimentos = document.querySelectorAll(".depoimentos-info li");
-let contador = 0;
-
-if (contador > 0) {
-  anterior.setAttribute("src", "assets/icones/slide-controller.svg");
-}
-
-if (window.innerWidth > 720) {
-  proximo.addEventListener("click", () => {
-    console.log("aa");
-    listaDepoimentos[contador].classList.remove("mostrar");
-    listaDepoimentos[listaDepoimentos.length - 1].classList.add("mostrar");
-    contador++;
-    if (contador > 0) {
-      anterior.setAttribute("src", "assets/icones/slide-controller.svg");
-    }
-    // anterior.setAttribute("img", "../assets/icons/slide-controller.svg");
-  });
-
-  anterior.addEventListener("click", () => {
-    console.log("oi");
-    listaDepoimentos[listaDepoimentos.length - 1].classList.remove("mostrar");
-    listaDepoimentos[contador - 1].classList.add("mostrar");
-    contador--;
-    if (contador <= 0) {
-      anterior.setAttribute("src", "assets/icones/seta-longa-cinza.svg");
-    }
-  });
-}
